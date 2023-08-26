511970e63ea28a371a8a004c3d8224c5262767d5
umekochan <masuda.ff@gmail.com>
2023-08-27 02:30:34 +0900

    ログイン実装 step2（エラー措置）

--- login.php
+++ login.php
@@ -1,24 +1,51 @@
-<?php 
+<?php
+
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+    ////ユーザー名の入力チェック
+    if( empty($name) ) {
+        echo "ユーザー名が入力されていません。";
+        return;
+    }
+    //パスワードの入力チェック
+    if( empty($password) ) {
+        echo "パスワードが入力されていません。";
+        return;
+    }
+
     //db接続情報
     $db_name = "mysql:host=localhost; dbname=yuuridb;";
     $db_username = "root";
     $db_password = "";
 
     //db接続
-    $db = new PDO($db_name, $db_username, $db_password);
+    try {
+        $db = new PDO($db_name, $db_username, $db_password);
+    } catch( PDOException $e ) {
+        //エラー処理
+        $msg = $e->getMessage();
+        echo "DB接続エラー__Error:";
+        echo $msg;
+        exit;
+    }
 
     //SQL文の定義
     $sql = "SELECT * FROM login WHERE name = :name";
     //SQLステートメントの準備
     $statement = $db->prepare($sql);
-    $statement->bindValue(':name', $_POST['name']);
+    $statement->bindValue(':name', $name);
     //SQL実行
     $statement->execute();
     //結果の取得
     $result = $statement->fetch();
 
+    if( !$result ) {
+        echo "無効なユーザーです。";
+        return;
+    }
+
     //パスワードが一致するか
-    if ( $_POST['password'] === $result['password'] ) {
+    if ( $password === $result['password'] ) {
         echo '<p>ログインしました。</p>';
     } else {
         echo '<p>パスワードが間違っています。</p>';
